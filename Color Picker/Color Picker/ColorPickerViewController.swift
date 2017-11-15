d6486a37d3250e9ec93716ee8b8b17be89ad9cdc
Teddy <teddyivanov95@gmail.com>
2017-11-14 18:08:22 -0600

    Implemented Color Picker Challenge

--- Color Picker/Color Picker/ColorPickerViewController.swift
+++ Color Picker/Color Picker/ColorPickerViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController {
-
+class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var colorPickerView: UIPickerView!
+    
+    //Red, Orange, Yellow, Green, Blue and Purple.
+    @IBOutlet weak var cololLabel: UILabel!
+    let colors: [(color: UIColor, name: String)] = [(.red, "Red"), (.orange, "Orange"), (.yellow, "Yellow"), (.green, "Green"), (.blue, "Blue"), (.purple, "Purple")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = colors[0].color
+        cololLabel.text = colors[0].name
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].name
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.view.backgroundColor = colors[row].color
+        self.cololLabel.text = colors[row].name
     }
     
 
